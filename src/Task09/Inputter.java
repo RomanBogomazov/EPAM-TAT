49101a57642bcf4002278819529bbbbeaf8d4194
unknown <timofeikyksenok>
2017-03-02 00:47:16 +0300

    changing files

--- src/Task09/Inputter.java
+++ src/Task09/Inputter.java
@@ -1,24 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package task9;
+package task09;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- *
- * @author mini
+ * Class Inputter
  */
 public class Inputter {
-    public String input() throws IOException{
-        System.out.println("Input time and date in format: (day)/(month)/(year) (hour):(minutes):(seconds)");
-        System.out.println("Examplle: 21/3/2000 12:01:07");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String string = reader.readLine();
-        return string;
-    }
+
+  /**
+   * Method output format date and examples of date and return inputting string
+   * return String @string - date which user input
+   */
+  public String input() throws IOException{
+    System.out.println("Input time and date in format: (day)/(month)/(year) (hour):(minutes):(seconds)");
+    System.out.println("Example: 21/3/2000 12:01:07");
+    System.out.println("Example: 2/3/245000 1:01:00");
+    System.out.println("Example: 21/8/20 2:1:7");
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    String string = reader.readLine();
+    return string;
+  }
 }
--- src/Task09/Tokenizator.java
+++ src/Task09/Tokenizator.java
@@ -1,26 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package task9;
+package task09;
 
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 /**
- *
- * @author mini
+ * Class Tokenizator
  */
 public class Tokenizator {
-    private ArrayList list = new ArrayList();
-    private String stringWithSpecSymbols = " /:";
-    public ArrayList tokenizeOnSpecSymbols(String string) {
-        StringTokenizer stringTokenizer = new StringTokenizer(string,stringWithSpecSymbols);
-        while(stringTokenizer.hasMoreTokens()) {
-            list.add(stringTokenizer.nextToken());
-        }
-        return list;
+  private ArrayList list = new ArrayList();
+  private String stringWithSpecSymbols = " /:";
+
+  /**
+   * Method tokenizating inputting string on spec symbols and return list strings between spec symbols
+   * params String @string - string for separating
+   * return ArrayList @list - strings between spec symbols
+   */
+  public ArrayList tokenizeOnSpecSymbols(String string) {
+    StringTokenizer stringTokenizer = new StringTokenizer(string,stringWithSpecSymbols);
+    while(stringTokenizer.hasMoreTokens()) {
+      list.add(stringTokenizer.nextToken());
     }
+    return list;
+  }
 }
  
