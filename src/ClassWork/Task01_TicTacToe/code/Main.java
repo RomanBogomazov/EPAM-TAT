fb12d0c5c1e75a0176b1d61776387234a19d55d0
timofeikyksenok <timofei.kyksenok@gmail.com>
2017-03-24 00:14:59 +0300

    upload files from classwork

---
+++ src/ClassWork/Task01_TicTacToe/code/Main.java
@@ -0,0 +1,12 @@
+package code;
+
+/**
+ * Created by timofeiKyksenok on 23.03.2017.
+ */
+public class Main {
+
+  public static void main(String[] args) {
+
+  }
+
+}
---
+++ src/ClassWork/Task01_TicTacToe/code/Player.java
@@ -0,0 +1,9 @@
+package code;
+
+/**
+ * Created by User on 23.03.2017.
+ */
+public class Player {
+    private char playerSymbol;
+
+}
