249317d3fe31d236f81a5e75c2e916f75908a1fd
Bogomazov Roman <bog.rom1593@mail.ru>
2017-03-27 14:11:36 +0300

    Creating Dev-14

--- Dev-12/main/java/Checker.java
+++ Dev-12/main/java/Checker.java
@@ -2,71 +2,62 @@
  * Class to check conditions.
  */
 class Checker{
-    private int[] curr = new int[2];
-    private int[] wish = new int[2];
+    private int[] curr = new int[2];    //array consist coordinates of current position in ASCII. example for "a1": curr[0] = 98, curr[1] = 49.
+    private int[] wish = new int[2];    //array consist coordinates of wishful position in ASCII.
+    private boolean answer = false;
 
 /**
- * Check figures movement on one side.
  * @param color is figure color.
  * @param current is current figure position.
  * @param wishful is wishful figure position.
- * @return 
+ * @return answer on a question: "Can sword movement if it has choosing color?"
  */
   boolean get(String color, String current, String wishful) {
-      curr[0] = current.charAt(0);
-      wish[0] = wishful.charAt(0);
-      curr[1] = current.charAt(1) - 48;
-      wish[1] = wishful.charAt(1) - 48;
+      curr[0] = current.charAt(0);      //current symbol of position in ASCII, for example: 'a' in 'a1'
+      wish[0] = wishful.charAt(0);      //wishful symbol
+      curr[1] = current.charAt(1) - 48;     //current number of position in ASCII, for example: '1' in 'a1'. magic number '48' - number of symbol
+      wish[1] = wishful.charAt(1) - 48;     //wishful number
 
       if (canMovement(curr, wish) == true & canLive(curr) == true & canLive(wish) == true) {
           if (color.equals("w")) {
               if (wish[1] > curr[1]) {
-                  return true;
-              }
-              else  {
-                  return false;
+                  answer = true;
               }
           }
           else {
               if (wish[1] < curr[1]) {
-                  return true;
-              }
-              else  {
-                  return false;
+                  answer = true;
               }
           }
       }
-      else {
-          return false;
-      }
+      return answer;
   }
 
   /**
-   * Check inputting coordinate on valid data.
    * @param coordinate is checker coordinates first: a-h and second: 1-8.
-   * @return
+   * @return answer on a question: "Can sword live?"
    */
   boolean canLive(int[] coordinate) {
+      boolean answer = false    ;
       if ((coordinate[0] + coordinate[1]) % 2 == 0) {
-          return true;
+          answer = true;
       }
-      else {
-          return false;
-      }
+      return answer;
   }
 
   /**
-   * Check coordinates on movement from one coordinate on second.
    * @param current current coordinates of checker.
    * @param wishful wishful coordinates of checker.
-   * @return
+   * @return answer on a question: "Can sword movement?"
    */
   boolean canMovement(int[] current, int[] wishful) {
-      if (Math.abs(wishful[0] - current[0]) <= Math.abs(wishful[1] - current[1])) {
-          return true;
+      boolean answer = false;
+      int absoluteNumberOfCoordinate = Math.abs(wishful[1] - current[1]);
+      int absoluteSymbolOfCoordinate = Math.abs(wishful[0] - current[0]);
+
+      if (absoluteSymbolOfCoordinate <= absoluteNumberOfCoordinate) {
+          answer = true;
       }
-      else {
-          return false;
-      }
+      return answer;
   }
 }
